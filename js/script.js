73d1fd2f45675cfe76ba89241cf356c8a465fc72
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-08-06 17:31:12 -0500

    Fixed: Carrousel bug

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 //CARROUSEL
-const slides = document.querySelectorAll('#hero .slide');
+const carousel = document.querySelector('.carousel');
+const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
 
-function nextSlide() {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
+function showSlide(n) {
+    currentSlide = (n + slides.length) % slides.length;
+    carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
 
-slides[currentSlide].classList.add('active');
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
 setInterval(nextSlide, 5000);
 
 
